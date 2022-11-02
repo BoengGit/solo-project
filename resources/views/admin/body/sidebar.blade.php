b1fe4ed00551c416e2f9ce8bc8e836c2215c9236
BoengGit <edukasiasqor@gmail.com>
2022-11-03 00:19:39 +0700

    wip

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -10,8 +10,8 @@
         <!-- User details -->
         <div class="user-profile text-center mt-3">
             <div class="">
-                <img src="{{ !empty($admindata->profile_image) ? url('upload/admin_images/' . $admindata->profile_image) : url('upload/no_image.png') }}" alt=""
-                    class="avatar-md rounded-circle">
+                <img src="{{ !empty($admindata->profile_image) ? url('upload/admin_images/' . $admindata->profile_image) : url('upload/no_image.png') }}"
+                    alt="" class="avatar-md rounded-circle">
             </div>
             <div class="mt-3">
                 <h4 class="font-size-16 mb-1">{{ $admindata->name }}</h4>
@@ -34,48 +34,26 @@
                 </li>
 
                 <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="ri-eraser-fill"></i>
+                        <span class="badge rounded-pill bg-danger float-end">1</span>
+                        <span>Forms</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="form-elements.html">Form Elements</a></li>
+                    </ul>
+                </li>
+
+                <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-mail-send-line"></i>
                         <span>Email</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="email-inbox.html">Inbox</a></li>
-                        <li><a href="email-read.html">Read Email</a></li>
                     </ul>
                 </li>
 
-                <li class="menu-title">Pages</li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-account-circle-line"></i>
-                        <span>Authentication</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="auth-login.html">Login</a></li>
-                        <li><a href="auth-register.html">Register</a></li>
-                        <li><a href="auth-recoverpw.html">Recover Password</a></li>
-                        <li><a href="auth-lock-screen.html">Lock Screen</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="waves-effect">
-                        <i class="ri-eraser-fill"></i>
-                        <span class="badge rounded-pill bg-danger float-end">8</span>
-                        <span>Forms</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="form-elements.html">Form Elements</a></li>
-                        <li><a href="form-validation.html">Form Validation</a></li>
-                        <li><a href="form-advanced.html">Form Advanced Plugins</a></li>
-                        <li><a href="form-editors.html">Form Editors</a></li>
-                        <li><a href="form-uploads.html">Form File Upload</a></li>
-                        <li><a href="form-xeditable.html">Form X-editable</a></li>
-                        <li><a href="form-wizard.html">Form Wizard</a></li>
-                        <li><a href="form-mask.html">Form Mask</a></li>
-                    </ul>
-                </li>
             </ul>
         </div>
         <!-- Sidebar -->
